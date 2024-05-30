1bfc964d0c6aa44bd2cb43a2d27553082cc5aaa0
MaryL <marishka61.88@gmail.com>
2024-05-30 19:52:57 +0300

    WebPack

---
+++ src/js/game.js
@@ -0,0 +1,23 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+};
+  
+class GameSavingData {
+};
+  
+function readGameSaving() {
+};
+  
+function writeGameSaving() {
+};
+
+const {Character} = require('./domain.js');
+
+module.exports = {
+    Game,
+    GameSavingData,
+    readGameSaving,
+    writeGameSaving
+}
